74d3641456af0b879566aa503e0d1c1a434126ef
Sam <istarshieldsi@gmail.com>
2025-09-05 17:23:03 -0500

    to laptop

--- Assets/_Game/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/_Game/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class HeroKnight : MonoBehaviour {
 
+
     public static HeroKnight Instance;
     [SerializeField] float      m_speed = 4.0f;
     [SerializeField] float      m_jumpForce = 7.5f;
@@ -19,20 +20,36 @@ public class HeroKnight : MonoBehaviour {
     private Sensor_HeroKnight   m_wallSensorL2;
     private bool                m_isWallSliding = false;
     private bool                m_grounded = false;
-    private bool                m_rolling = false;
     private int                 m_facingDirection = 1;
     private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
-    private float               m_rollDuration = 8.0f / 14.0f;
-    private float               m_rollCurrentTime;
 
-    
+    public SpriteRenderer spriteRenderer;
+
+    [System.Serializable] public enum CurCharacter
+    {
+        None,
+        Edrick,
+        Yael,
+        Jasper
+    }
+ public CurCharacter _curCharacter;
+    private int charaIndex;
+    private int minchara = 0;
+    private int maxchara = 2;
+    public Sprite[] _charaSprite;
+    public bool jasperAvailable;
+    public bool yaelAvailable;
+    public Transform yaelCheck;
+    public Transform japserCheck;
+    public Vector2 dist; 
+    public GameObject[] Party;
 
 
     // Use this for initialization
     void Start ()
     {
+       
+        _curCharacter = CurCharacter.Edrick;
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
@@ -57,16 +74,48 @@ public class HeroKnight : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        // Increase timer that controls attack combo
-        m_timeSinceAttack += Time.deltaTime;
+        if(charaIndex > maxchara)
+        {
+            charaIndex = minchara;
+        }
+        if(charaIndex < minchara)
+        {
+            charaIndex = maxchara;
+        }
+        if(charaIndex == 0)
+        {
+            _curCharacter = CurCharacter.Edrick;
+        }
+        if(charaIndex == 1 && yaelAvailable)
+        {
+            _curCharacter = CurCharacter.Yael;
+        }
+        if(charaIndex == 2 && jasperAvailable)
+        {
+            _curCharacter = CurCharacter.Jasper;
+        }
+        switch (_curCharacter)
+        {
+            case CurCharacter.Edrick:
+                spriteRenderer.sprite = _charaSprite[0];
+                break;
+            case CurCharacter.Yael:
+                spriteRenderer.sprite = _charaSprite[1];
+                break;
+            case CurCharacter.Jasper:
+                spriteRenderer.sprite = _charaSprite[2];
+                break;
+            case CurCharacter.None:
+                break;
+            default:
+                Debug.Log("unknown character.");
+                break;
+        }
+
+
 
         // Increase timer that checks roll duration
-        if(m_rolling)
-            m_rollCurrentTime += Time.deltaTime;
-
-        // Disable rolling if timer extends duration
-        if(m_rollCurrentTime > m_rollDuration)
-            m_rolling = false;
+      
 
         //Check if character just landed on the ground
         if (!m_grounded && m_groundSensor.State())
@@ -99,7 +148,6 @@ public class HeroKnight : MonoBehaviour {
         }
 
         // Move
-        if (!m_rolling )
             m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
 
         //Set AirSpeed in animator
@@ -111,18 +159,36 @@ public class HeroKnight : MonoBehaviour {
         m_animator.SetBool("WallSlide", m_isWallSliding);
 
         //Death
-        if (Input.GetKeyDown("e") && !m_rolling)
+        if (Input.GetKeyDown("e"))
         {
-            m_animator.SetBool("noBlood", m_noBlood);
-            m_animator.SetTrigger("Death");
+            Debug.Log("SwapRight");
+            SwapRight();
         }
             
         //Hurt
-        else if (Input.GetKeyDown("q") && !m_rolling)
-            m_animator.SetTrigger("Hurt");
+        else if (Input.GetKeyDown("q"))
+        {
+     Debug.Log("SwapLeft");
+            SwapLeft();
 
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Placing Companion");
+            
+            if(charaIndex == 1)
+            {
+                instantiatedChara = Party[0];
+            }
+            if(charaIndex == 2)
+            {
+                instantiatedChara = Party[1];
+            }
+            PlaceCompanion();
+        }
+       
         //Attack
-        else if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling)
+        else if(Input.GetMouseButtonDown(0))
         {
             m_currentAttack++;
 
@@ -131,18 +197,18 @@ public class HeroKnight : MonoBehaviour {
                 m_currentAttack = 1;
 
             // Reset Attack combo if time since last attack is too large
-            if (m_timeSinceAttack > 1.0f)
+      
                 m_currentAttack = 1;
 
             // Call one of three attack animations "Attack1", "Attack2", "Attack3"
             m_animator.SetTrigger("Attack" + m_currentAttack);
 
             // Reset timer
-            m_timeSinceAttack = 0.0f;
+          
         }
 
         // Block
-        else if (Input.GetMouseButtonDown(1) && !m_rolling)
+        else if (Input.GetMouseButtonDown(1))
         {
             m_animator.SetTrigger("Block");
             m_animator.SetBool("IdleBlock", true);
@@ -152,16 +218,16 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetBool("IdleBlock", false);
 
         // Roll
-        else if (Input.GetKeyDown("left shift") && !m_rolling && !m_isWallSliding)
+        else if (Input.GetKeyDown("left shift") && !m_isWallSliding)
         {
-            m_rolling = true;
+          
             m_animator.SetTrigger("Roll");
             m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
         }
             
 
         //Jump
-        else if (Input.GetKeyDown("space") && m_grounded && !m_rolling)
+        else if (Input.GetKeyDown("space") && m_grounded)
         {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
@@ -174,22 +240,41 @@ public class HeroKnight : MonoBehaviour {
         else if (Mathf.Abs(inputX) > Mathf.Epsilon)
         {
             // Reset timer
-            m_delayToIdle = 0.05f;
-            m_animator.SetInteger("AnimState", 1);
+         
+           // m_animator.SetInteger("AnimState", 1);
         }
 
         //Idle
         else
         {
-            // Prevents flickering transitions to idle
-            m_delayToIdle -= Time.deltaTime;
-                if(m_delayToIdle < 0)
-                    m_animator.SetInteger("AnimState", 0);
+           
         }
     }
 
     // Animation Events
     // Called in slide animation.
+
+    void SwapRight()
+    {
+        Debug.Log("Swapping CH Right");
+        charaIndex += 1;
+        
+    }
+    void SwapLeft()
+    {
+        Debug.Log("Swapping CH Left");
+        charaIndex -= 1;
+    }
+    void PlaceCompanion()
+    {
+        if(charaIndex != 0)
+        {
+            Debug.Log("Placing Companion: " + _curCharacter);
+        }
+        Party[charaIndex].SetActive(true);
+        Party[charaIndex].transform.parent = null;
+
+    }
     void AE_SlideDust()
     {
         Vector3 spawnPosition;
