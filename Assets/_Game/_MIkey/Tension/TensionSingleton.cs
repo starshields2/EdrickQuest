6a95627283209627e6e1798825058d5354151e0e
Mikey Bess <mikeybess27@gmail.com>
2026-04-06 14:26:37 -0500

    Room changes

--- Assets/_Game/_MIkey/Tension/TensionSingleton.cs
+++ Assets/_Game/_MIkey/Tension/TensionSingleton.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class TensionSingleton : MonoBehaviour
 {
-private int _tensionLevel = 0;
-public int TensionLevel
-{
-    get { return _tensionLevel; }
-    set { _tensionLevel = value; }
-}
+    private int _tensionLevel = 0;
+    public int TensionLevel
+    {
+        get { return _tensionLevel; }
+        set { _tensionLevel = value; }
+    }
+
+    private string _previousScene;
+    public string PreviousScene
+    {
+        get { return _previousScene; }
+        set { _previousScene = value; }
+    }
+
     public static TensionSingleton Instance { get; private set; }
     private void Awake()
     {
@@ -27,5 +35,10 @@ public int TensionLevel
     public void ClampTension()
     {
         _tensionLevel = Mathf.Clamp(_tensionLevel, 0, 23);
+    }
+
+    public void SetPreviousScene()
+    {
+        _previousScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
     }
 }
